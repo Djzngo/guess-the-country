50e8459c927111e8400e95a5fafa2bcf0392b074
Djzngo <nimi_9@hotmail.co.uk>
2026-08-26 19:25:34 +0100

    added game restart function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,8 @@
-const API_URL = "https://api.restcountries.com/countries/v5?region=europe"
+const API_URL = "https://api.restcountries.com/countries/v5?limit=100&region=europe"
 const API_KEY = "Bearer rc_live_79b6961d3d694fd19986cb45e54a3983";
 
 document.getElementById("btnStart").addEventListener("click", (e) => startGame(e));
+document.getElementById("btnRestart").addEventListener("click", () => gameRestart());
 document.getElementById("btnSubmit").addEventListener("click", () => userSubmit())
 
 let game = {
@@ -31,8 +32,8 @@ async function getRequest(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data);
-        rndCountry(data);
+        generateCountry(data);
+        selectCountry();
         
     }
     else {
@@ -53,18 +54,43 @@ function startGame (e) {
     game.guesses = 0;
     game.answer = null;
 
+    //Runs API for country data.
     getRequest(e);
 
 }
 
+// Empties answers object, and select a new country to guess
+function gameRestart() {
+    game.guesses = 0;
+    showGuesses();
+    
+
+    Object.keys(answers).forEach((i) => {
+        answers[i] = null;
+    });
+
+    selectCountry();
+
+}
+
+// Selects a random country from game.country array to be placed in answers.country
+function selectCountry() {
+    let rndNum = Math.floor(Math.random() * 53);
+
+    answers.country = game.country[rndNum];
+    answers.region = game.region[rndNum];
+    answers.currency = game.currency[rndNum];
+    console.log(answers);
+}
+
 // push country data to arrays within game object
-function rndCountry(data) {
+function generateCountry(data) {
     const array = data;
     let countries = game.country;
     let region = game.region;
     let currency = game.currency;
 
-    for (let i = 0; i <= 10; i++) {
+    for (let i = 0; i <= 53; i++) {
         countries.push(array.data.objects[i].names.common);
         region.push(array.data.objects[i].region);
         // If coutry does not have currency listed push 'N/A' in its place
@@ -74,17 +100,19 @@ function rndCountry(data) {
         else {
             currency.push(array.data.objects[i].currencies[0].code);
         }
-        console.log(game);
-    }
+    };
 }
 
 function userSubmit() {
     let guess = document.getElementById("txtGuess").innerText;
+    let answer = answers.country
     
-    if (guess != game.answer) {
+    if (guess != answer) {
         game.guesses += 1;
         showGuesses();
-        console.log(game.guesses);
+    }
+    else {
+        console.log("correct")
     }
 
     
@@ -92,10 +120,6 @@ function userSubmit() {
 
 function showGuesses() {
     document.getElementById("guessCount").innerText = game.guesses;
-}
-
-function gameRestart() {
-
 }
 
 function btnControl() {
